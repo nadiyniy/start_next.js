e82f69de3d33e2c76496759ee1aa783ad0ec32a4
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-12 14:51:20 +0200

    endpoint

---
+++ app/api/posts/posts.tsx
@@ -0,0 +1,62 @@
+export const posts = [
+  {
+    id: "1",
+    title: "Lorem Ipsum 1",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2023, 0, 15), // Random date: January 15, 2023
+  },
+  {
+    id: "2",
+    title: "Lorem Ipsum 2",
+    body: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    date: new Date(2023, 2, 7), // Random date: March 7, 2023
+  },
+  {
+    id: "3",
+    title: "Lorem Ipsum 3",
+    body: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    date: new Date(2023, 4, 22), // Random date: May 22, 2023
+  },
+  {
+    id: "4",
+    title: "Lorem Ipsum 4",
+    body: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: new Date(2023, 6, 11), // Random date: July 11, 2023
+  },
+  {
+    id: "5",
+    title: "Lorem Ipsum 5",
+    body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    date: new Date(2023, 8, 5), // Random date: September 5, 2023
+  },
+  {
+    id: "6",
+    title: "Lorem Ipsum 6",
+    body: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    date: new Date(2023, 10, 18), // Random date: November 18, 2023
+  },
+  {
+    id: "7",
+    title: "Lorem Ipsum 7",
+    body: "Cras euismod vestibulum lectus, id posuere turpis congue a.",
+    date: new Date(2023, 1, 9), // Random date: February 9, 2023
+  },
+  {
+    id: "8",
+    title: "Lorem Ipsum 8",
+    body: "Nunc ut sem vel neque malesuada tincidunt.",
+    date: new Date(2023, 3, 26), // Random date: April 26, 2023
+  },
+  {
+    id: "9",
+    title: "Lorem Ipsum 9",
+    body: "Phasellus non dictum arcu, sit amet ullamcorper nisi.",
+    date: new Date(2023, 5, 14), // Random date: June 14, 2023
+  },
+  {
+    id: "10",
+    title: "Lorem Ipsum 10",
+    body: "Vivamus ultricies, ligula vitae fringilla ullamcorper, erat nisi rhoncus turpis, vel efficitur nulla turpis sit amet lectus.",
+    date: new Date(2023, 7, 30), // Random date: August 30, 2023
+  },
+];
--- app/services/getposts.tsx
+++ app/services/getposts.tsx
@@ -1,5 +1,8 @@
 export const getAllPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = await fetch(
+    // "https://jsonplaceholder.typicode.com/posts"
+    "api/posts"
+  );
 
   if (!response.ok) throw new Error("Unable to fetch posts.");
 
@@ -8,7 +11,8 @@ export const getAllPosts = async () => {
 
 export const getPostsBySearch = async (search: string) => {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?q=${search}`
+    // `https://jsonplaceholder.typicode.com/posts?q=${search}`
+    `api/posts?query=${search}`
     // { next: { revalidate: 60 } }
   );
 
