35d29d2af9c6aae7755337c501cf05f87a3f6d89
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-20 12:22:36 +0200

    fix format image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = nextConfig;
 
 module.exports = {
   images: {
+    formats: ["images/avif", "image/webp"],
+    remotePatterns: [],
     domains: ["lh3.googleusercontent.com", "randomuser.me"],
   },
 };
